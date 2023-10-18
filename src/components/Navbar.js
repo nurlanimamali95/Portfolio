68c385c28820f5fc6d7b87b104ec6eede32e990b
Nurlan <nurlanimamali@gmail.com>
2023-10-18 20:25:16 +0200

    Initial Commit and Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1024px] mx-auto px-4">
+      <h1 className="w-full text-3xl font-bold ">NI.</h1>
+      <ul className="hidden md:flex">
+        <li className="p-4">Projects</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#56655f] ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full text-3xl font-bold m-4">NI.</h1>
+        <ul className="uppercase p-4">
+          <li className="p-4 border-b border-buttery">Projects</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        buttery: "#e5fddb",
+      },
+    },
+  },
+  plugins: [],
+};
