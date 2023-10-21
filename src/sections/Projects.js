da708f4691f635f55c4fec3e0abb240677f2a11c
Nurlan <nurlanimamali@gmail.com>
2023-10-21 15:31:07 +0200

    Finished with projects section

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Card from "../components/Card";
+import Card1 from "../assets/AlbumSpotter.png";
+import Card2 from "../assets/ArtMuseum.png";
+import Card3 from "../assets/BookYourFuture.png";
+import Card4 from "../assets/MarioQuiz (2).png";
 
 const Projects = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-nurlan">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 gap-20">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="max-w-[1500px] mx-auto grid md:grid-cols-2 gap-20">
+        <Card image={Card1} />
+        <Card image={Card2} />
+        <Card image={Card3} />
+        <Card image={Card4} />
       </div>
     </div>
   );
