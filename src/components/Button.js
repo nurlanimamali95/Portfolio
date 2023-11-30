bf45dee7ea1e0e918af6c71828a5312e128dd670
Nurlan <nurlanimamali@gmail.com>
2023-11-30 19:36:02 +0100

    Button design and animation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const hoverVariants = {
+  hidden: {
+    scale: 1,
+  },
+  visible: {
+    scale: 1.1,
+    transition: {
+      type: "spring",
+      stiffness: 500,
+      ease: "linear",
+      
+    },
+  },
+};
 
 const Button = (props) => {
   return (
-    <div>
-      <button
+    <motion.div className="m-6">
+      <motion.button
         onClick={props.onClick}
-        className="bg-[#00df9a] text-nurlan w-[200px] rounded-md font-medium mx-auto px-6 py-3  hover:bg-black hover:text-[#00df9a] my-6  md:mx-0"
+        className="bg-[#00df9a] text-nurlan w-[150] rounded-md font-medium mx-auto px-6 py-3 hover:bg-black hover:text-[#00df9a] my-6 md:mx-0"
+        variants={hoverVariants}
+        whileHover="visible"
       >
         <Link to={props.url}>{props.title}</Link>
-      </button>
-    </div>
+      </motion.button>
+    </motion.div>
   );
 };
 
