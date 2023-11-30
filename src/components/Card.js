00a35679c2732b7969da0eaecb52a796ca73aa92
Nurlan <nurlanimamali@gmail.com>
2023-11-30 19:37:00 +0100

    framer motion animation for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,66 @@
 import React from "react";
 import Button from "./Button";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 
 const Card = (props) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const mouseXSpring = useSpring(x);
+  const mouseYSpring = useSpring(y);
+
+  const rotateX = useTransform(
+    mouseYSpring,
+    [-0.5, 0.5],
+    ["17.5deg", "-17.5deg"]
+  );
+  const rotateY = useTransform(
+    mouseXSpring,
+    [-0.5, 0.5],
+    ["-17.5deg", "17.5deg"]
+  );
+
+  const handleMouseMove = (e) => {
+    const rect = e.target.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const xPct = mouseX / width - 0.5;
+    const yPct = mouseY / height - 0.5;
+
+    x.set(xPct);
+    y.set(yPct);
+  };
+
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
   return (
-    <div className="">
-      <div className="w-full h-500 shadow-xl flex flex-col p-4 my-4 rounded-lg ">
-        <div className="inline-block h-80 mx-auto mt-[-3rem] bg-white">
-          <img
-            src={props.image}
-            className="w-full h-full object-cover"
-            alt="Project Screenshot"
-          />
-        </div>
-        <div className="flex justify-center">
-          <Button className="" title="Take a look" />
-        </div>
+    <motion.div className="w-full h-500 shadow-xl flex flex-col p-4 my-4 rounded-lg ">
+      <div className="inline-block h-80 mx-auto mt-[-3rem] ">
+        <motion.img
+          src={props.image}
+          className="w-full h-full object-cover curs"
+          alt="Project Screenshot"
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
+          style={{
+            rotateY,
+            rotateX,
+            transformStyle: "preserve-3d",
+          }}
+        />
       </div>
-    </div>
+      <div className="flex justify-center">
+        <Button className="" title="Demo" />
+        <Button className="" title="Source" />
+      </div>
+    </motion.div>
   );
 };
 
