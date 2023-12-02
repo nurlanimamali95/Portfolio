fc5018139a325d4d85e30c9cbea19aa0c8c146fc
Nurlan <nurlanimamali@gmail.com>
2023-12-02 13:09:12 +0100

    Fomr background changed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,19 @@
 import React from "react";
+import Button from "./Button";
 
 const Contact = () => {
   return (
     <div>
-      <section className="text-gray-700 body-font relative">
+      <section className="bg-buttery text-black">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Contact Us
+              Contact Me
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify.
+              Let's connect! Drop me a message using the form below, or feel
+              free to reach out through any of the social links provided. I look
+              forward to hearing from you!
             </p>
           </div>
           <div className="lg:w-1/2 md:w-2/3 mx-auto">
@@ -56,22 +58,18 @@ const Contact = () => {
                   <textarea
                     id="message"
                     name="message"
-                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-nurlan h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                   ></textarea>
                 </div>
               </div>
               <div className="p-2 w-full">
-                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                  Button
-                </button>
+                <Button title="Send"/>
               </div>
               <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                <a className="text-indigo-500">example@email.com</a>
-                <p className="leading-normal my-5">
-                  49 Smith St.
-                  <br />
-                  Saint Cloud, MN 56301
-                </p>
+                <a href="nurlanimamali@gmail.com" className="underline">
+                  nurlanimamali@gmail.com
+                </a>
+                <p className="leading-normal my-5">Utrecht, Netherlands</p>
                 <span className="inline-flex">
                   <a className="text-gray-500">
                     <svg
