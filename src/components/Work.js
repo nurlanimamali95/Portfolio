dcd280ea889b9852a9cdf86fff4a4bf32a1fb97f
Nurlan <nurlanimamali@gmail.com>
2023-12-02 13:09:49 +0100

    Work section height changed

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,7 @@ import Button from "../components/Button";
 
 const Work = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center">
       <h3 className="md:text-5xl sm:text-4xl text-3xl font-medium md:py-6">
         Liked what you just saw?
       </h3>
