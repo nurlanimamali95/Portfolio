1f26c50c2133c94a33e2f6d20d0d5ca1503357e9
Nurlan <nurlanimamali@gmail.com>
2023-12-05 18:43:51 +0100

    Form added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,140 +1,122 @@
-import React from "react";
-import Button from "./Button";
+import { useRef, useState } from "react";
+import { motion, useInView } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Contact = () => {
+  const ref = useRef();
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_94y20xo",
+        "template_v10u2oh",
+        formRef.current,
+        "pX_2hasGmGcuvjXIW"
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+        },
+        (error) => {
+          setError(true);
+        }
+      );
+  };
+
   return (
-    <div>
-      <section className="bg-buttery text-black">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Contact Me
-            </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Let's connect! Drop me a message using the form below, or feel
-              free to reach out through any of the social links provided. I look
-              forward to hearing from you!
-            </p>
-          </div>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <div className="flex flex-wrap -m-2">
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label for="name" className="leading-7 text-sm text-gray-600">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-              </div>
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label
-                    for="email"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-              </div>
-              <div className="p-2 w-full">
-                <div className="relative">
-                  <label
-                    for="message"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-nurlan h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
-                </div>
-              </div>
-              <div className="p-2 w-full">
-                <Button title="Send"/>
-              </div>
-              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                <a href="nurlanimamali@gmail.com" className="underline">
-                  nurlanimamali@gmail.com
-                </a>
-                <p className="leading-normal my-5">Utrecht, Netherlands</p>
-                <span className="inline-flex">
-                  <a className="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <rect
-                        width="20"
-                        height="20"
-                        x="2"
-                        y="2"
-                        rx="5"
-                        ry="5"
-                      ></rect>
-                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
-              </div>
-            </div>
-          </div>
+    <section className="h-screen mb-[6em]">
+      <div>
+        <h3 className="text-center text-4xl font-bold">Contact me</h3>
+      </div>
+      <motion.div
+        ref={ref}
+        className="h-full max-w-[1024px] flex gap-10 justify-between items-center mx-auto mb-10 px-4"
+        variants={variants}
+        initial="initial"
+        whileInView="animate"
+      >
+        <motion.div className="flex-1 flex flex-col gap-10" variants={variants}>
+          <motion.h1 className="text-2xl font-bold" variants={variants}>
+            Liked what you just saw?
+          </motion.h1>
+          <motion.div variants={variants}>
+            <h2 className="text-lg font-semibold">Mail Me</h2>
+            <span className="text-gray-600 hover:text-buttery transition duration-700 ease-in-out">
+              nurlanimamali@gmail.com
+            </span>
+          </motion.div>
+          <motion.div variants={variants}>
+            <h2 className="text-lg font-semibold">Call Me</h2>
+            <span className="text-gray-600 hover:text-buttery duration-700 ease-in-out  ">
+              +31627623727
+            </span>
+          </motion.div>
+        </motion.div>
+        <div className="flex-1 relative">
+          <motion.div
+            className="phoneSvg"
+            initial={{ opacity: 1 }}
+            whileInView={{ opacity: 0 }}
+            transition={{ delay: 3, duration: 1 }}
+          ></motion.div>
+          <motion.form
+            ref={formRef}
+            onSubmit={sendEmail}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            className="bg-buttery p-6 rounded-md shadow-md"
+          >
+            <input
+              type="text"
+              required
+              placeholder="Name"
+              name="name"
+              className="text-nurlan block w-full border-gray-300 rounded-md p-2 mb-4 "
+            />
+            <input
+              type="email"
+              required
+              placeholder="Email"
+              name="email"
+              className="text-nurlan block w-full p-2 mb-4"
+            />
+            <textarea
+              rows={8}
+              placeholder="Message"
+              name="message"
+              className=" text-nurlan block w-full border-gray-300 rounded-md p-2 mb-4 resize-none"
+            ></textarea>
+            <button className="bg-nurlan font-bold py-2 px-4 rounded-md hover:bg-buttery">
+              Submit
+            </button>
+            {error && <p className="text-red-500">Error</p>}
+            {success && <p className="text-green-500">Success</p>}
+          </motion.form>
         </div>
-      </section>
-    </div>
+      </motion.div>
+    </section>
   );
 };
 
