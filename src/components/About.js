9c5ee2bf78ef07e1327e2c4dfd2214b1a7375490
Nurlan <nurlanimamali@gmail.com>
2023-12-06 12:55:33 +0100

    structure is done

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Laptop from "../assets/Laptop4.jpg";
+import Laptop from "../assets/meillustration.png";
 
-const Analytics = () => {
+const About = () => {
   return (
-    <div className=" bg-buttery text-nurlan w-full  px-4">
+    <div className=" bg-buttery  text-nurlan w-full px-4">
       <div className="text-center">
-        <h1 className="md:text-4xl sm:text-3xl text-2xl font-medium py-[2em]">
+        <h1 className=" font-bold md:text-4xl sm:text-3xl text-2xl py-[2em]">
           A little bit about me
         </h1>
       </div>
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 py-1">
-        <img className="w-[500px] mx-auto my-4" src={Laptop} alt="/" />
-        <div className="flex flex-col justify-center">
+        <img className="w-[25em] mx-auto my-4" src={Laptop} alt="/" />
+        <div className="flex flex-col justify-center pb-[5em]">
           <h3 className="text-[#00df9a] font-medium md:text-2xl sm:text-1xl text-1xl my-4 py-2">
             Meet the Digital Wizard
           </h3>
-          <p>
+          <p className="text-xl max-w-[28em]">
             Greetings! I'm a web developer with a passion for creating
             captivating websites and seamless user experiences. My expertise
             spans both front-end and back-end development, merging creativity
@@ -33,4 +33,4 @@ const Analytics = () => {
   );
 };
 
-export default Analytics;
+export default About;
