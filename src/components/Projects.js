085812b9eaee2149b8ab59494916fb7fcd377193
Nurlan <nurlanimamali@gmail.com>
2023-12-15 19:29:14 +0100

    projects refactoring

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Card from "../components/Card";
 
-const cardImages = [
-  require("../assets/AlbumSpotter.png"),
-  require("../assets/ArtMuseum.png"),
-  require("../assets/BookYourFuture.png"),
-  require("../assets/MarioQuiz (2).png"),
+const projectDetails = [
+  {
+    image: require("../assets/AlbumSpotter.png"),
+    demoUrl: "https://nurlanimamali95.github.io/AlbumSpotter/",
+    sourceUrl: "https://github.com/nurlanimamali95/AlbumSpotter",
+  },
+  {
+    image: require("../assets/ArtMuseum.png"),
+    demoUrl:
+      "https://a-museum-react-m6dw-my6hzwkze-nurlanimamali95.vercel.app/",
+    sourceUrl: "https://github.com/nurlanimamali95/ArtMuseum",
+  },
+  {
+    image: require("../assets/BookYourFuture.png"),
+    demoUrl: "https://hyf-c43-group-1-c55b85dfa12d.herokuapp.com/login",
+    sourceUrl: "https://github.com/nurlanimamali95/BookYourFuture",
+  },
+  {
+    image: require("../assets/MarioQuiz (2).png"),
+    demoUrl: "https://nurlanimamali95.github.io/SuperMarioQuiz/",
+    sourceUrl: "https://github.com/nurlanimamali95/SuperMarioQuiz",
+  },
 ];
 
 const Projects = () => {
@@ -15,8 +32,13 @@ const Projects = () => {
         <h1 className="text-4xl font-bold py-2 ">Projects</h1>
       </div>
       <div className="max-w-[1500px] mx-auto grid md:grid-cols-2 gap-20">
-        {cardImages.map((image, index) => (
-          <Card key={index} image={image} />
+        {projectDetails.map((project, index) => (
+          <Card
+            key={index}
+            image={project.image}
+            demoUrl={project.demoUrl}
+            sourceUrl={project.sourceUrl}
+          />
         ))}
       </div>
     </div>
