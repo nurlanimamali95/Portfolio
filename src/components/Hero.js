6c37f151310495f2d87398ec5db1e66eb1db33ae
Nurlan <nurlanimamali@gmail.com>
2023-12-15 19:28:53 +0100

    animation to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { motion } from "framer-motion";
 import AnimatedScroll from "./AnimatedScroll";
 
 const Hero = () => {
+  const [startTyping, setStartTyping] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStartTyping(true);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const [typeEffect] = useTypewriter({
     words: [
+      "",
+      "",
       "HTML",
       "CSS",
       "JavaScript",
@@ -15,21 +28,33 @@ const Hero = () => {
       "Tailwind",
     ],
     loop: {},
-    typeSpeed: 100,
-    deleteSpeed: 60,
+    typeSpeed: 150,
+    deleteSpeed: 100,
+    start: startTyping,
   });
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6, delay: 0.5 } },
+  };
+
   return (
     <div className="text-buttery select-none">
       <div className="max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-        <h1 className="md:text-5xl sm:text-4xl text-3xl font-medium md:py-6">
+        <motion.h1
+          className="md:text-5xl sm:text-4xl text-3xl font-medium md:py-6"
+          initial="hidden"
+          animate="visible"
+          variants={textVariants}
+        >
           Welcome to My Portfolio I'm Nurlan Imamali, a Web Developer
-        </h1>
+        </motion.h1>
         <div className="flex justify-center items-center md:text-3xl sm:text-2xl text-xl m-5">
-          <p>
+          <motion.p variants={textVariants} initial="hidden" animate="visible">
             Creating Websites with{" "}
             <span className="text-[#00df9a]">{typeEffect}</span>
             <Cursor cursorStyle="❤️" cursorBlinking="false" />
-          </p>
+          </motion.p>
         </div>
         <div className="mt-[3em] sm:mt-[6em] mx-auto">
           <AnimatedScroll />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import Button from "../components/Button";
 import { Link as ScrollLink } from "react-scroll";
 import AnimatedLogo from "./AnimatedLogo";
 
@@ -41,24 +39,6 @@ const Navbar = () => {
           </ScrollLink>
         </li>
       </ul>
-      <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
-      </div>
-      <div
-        className={
-          nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#56655f] ease-in-out duration-500"
-            : "fixed left-[-100%]"
-        }
-      >
-        <h1 className="w-full text-3xl font-bold m-4">NI.</h1>
-        <ul className="uppercase p-4">
-          <li className="p-4 border-b border-buttery ">Projects</li>
-          <li className="p-4">
-            <Button title="Contact me" />
-          </li>
-        </ul>
-      </div>
     </div>
   );
 };
